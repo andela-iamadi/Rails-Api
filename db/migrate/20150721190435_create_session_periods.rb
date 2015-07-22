e7b97975fcbac7d0c5b17fb47ae80fb684f68b76
Tams Sokari <tams.sokari@andela.com>
2015-07-22 17:23:34 +0100

    Fixed the database issues

--- db/migrate/20150721190435_create_session_periods.rb
+++
@@ -1,9 +0,0 @@
-class CreateSessionPeriods < ActiveRecord::Migration
-  def change
-    create_table :session_periods do |t|
-      t.belongs_to :day, :index => true
-      t.belongs_to :session, :index => true
-      t.timestamps null: false
-    end
-  end
-end
